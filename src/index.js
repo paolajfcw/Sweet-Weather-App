cc9d9d9960bff6d243d3143d0f9a843ac28853dc
paolajfcw <67068066+paolajfcw@users.noreply.github.com>
2020-08-05 12:06:50 +0200

    Added the https link correctly

--- src/index.js
+++ src/index.js
@@ -42,7 +42,7 @@ function showWeatherInfo(response) {
   let iconElement = document.querySelector("#weather-icon");
   iconElement.setAttribute(
     "src",
-    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
